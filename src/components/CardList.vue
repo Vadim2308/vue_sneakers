3c99968eb26f6111477ef87c8d5021ca1bf401c9
Vadim_Niziyov <Vadim.Niziev@rt.ru>
2023-12-09 23:59:09 +0300

    initial

---
+++ src/components/CardList.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert('onClickAdd')
+}
+const onClickFavorite = () => {
+  alert('onClickFavorite')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      :isFavorite="true"
+      :isAdded="true"
+      :price="5000"
+      title="Nike"
+      imageUrl="/sneakers/sneakers-1.jpg"
+      :on-click-add="onClickAdd"
+      :on-click-favorite="onClickFavorite"
+    />
+    <Card :price="5000" title="Nike" imageUrl="/sneakers/sneakers-2.jpg" />
+    <Card :price="5000" title="Nike" imageUrl="/sneakers/sneakers-3.jpg" />
+    <Card :price="5000" title="Nike" imageUrl="/sneakers/sneakers-4.jpg" />
+    <Card :price="5000" title="Nike" imageUrl="/sneakers/sneakers-5.jpg" />
+    <Card :price="5000" title="Nike" imageUrl="/sneakers/sneakers-6.jpg" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps({
+  title: String,
+  price: Number,
+  isAdded: Boolean
+})
+</script>
+
+<template>
+  <div class="product">
+    <h1>{{ title }}</h1>
+    <h2>{{ price }}</h2>
+    <h3>{{ isAdded }}</h3>
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.product {
+  border: 1px solid blue;
+}
+</style>
