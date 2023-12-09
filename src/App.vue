ffd4818de1968a2d2ad929095008a03241158cdb
Vadim_Niziyov <st1mpro23@yandex.ru>
2023-12-10 01:45:12 +0300

    filters & drawer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
-import Drawer from './components/Drawer.vue'
 </script>
 
 <template>
